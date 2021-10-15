90479c6f7a45d2c63f5369563537a0d58fbc7f5c
jmalabed <jmalabed27@gmail.com>
2021-10-14 18:17:14 -0700

    edit for heroku

--- db/db.js
+++ db/db.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
-
-// Error / Disconnection
-mongoose.connection.on("error", (err) =>
-  console.log(err.message + " is Mongod not running?")
-);
-mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+const MONGODB_URI =
+  process.env.MONGODB_URI || `mongodb://localhost/officeCulture`;
 
 // Connection string (we will be replacing this later with environmental variables)
-mongoose.connect("mongodb://localhost:27017/officeCulture", {
+// Connect to Mongo
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 mongoose.connection.once("open", () => {
   console.log("connected to mongoose...");
 });
+
+// Error / Disconnection
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
