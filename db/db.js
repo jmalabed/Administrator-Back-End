80d593db0cf99e52c051586a668ee47879b959ec
jmalabed <jmalabed27@gmail.com>
2021-10-14 18:31:05 -0700

    updates for heroku

--- db/db.js
+++ db/db.js
@@ -1,19 +1,17 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const MONGODB_URI =
-  process.env.MONGODB_URI || `mongodb://localhost/officeCulture`;
-
-// Connection string (we will be replacing this later with environmental variables)
-// Connect to Mongo
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.connection.once("open", () => {
-  console.log("connected to mongoose...");
-});
+const DB_URL = process.env.DB_URL || `mongodb://localhost/officeCulture`;
 
 // Error / Disconnection
 mongoose.connection.on("error", (err) =>
   console.log(err.message + " is Mongod not running?")
 );
 mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+// Connect to Mongo
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
